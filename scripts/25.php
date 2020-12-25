695a28cba106e013323fe208475a067914f586ce
Steve Adams <irwtdvoys@gmail.com>
2020-12-25 21:41:39 +0000

    Implementation

---
+++ scripts/25.php
@@ -0,0 +1,13 @@
+<?php
+	define("ROOT", __DIR__ . "/../");
+
+	require_once(ROOT . "bin/init.php");
+
+	use App\Rfid;
+
+	$helper = new Rfid(25);//, ROOT . "data/25/example");
+	$helper->run()->output();
+
+	// Part 1: 7936032
+	// Part 2: N/A
+?>

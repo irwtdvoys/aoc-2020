66f25a68a283cc66e18209396aac5f6291d9a443
Steve Adams <irwtdvoys@gmail.com>
2021-09-13 15:41:40 +0100

    Clean up

--- bin/init.php
+++ bin/init.php
@@ -6,17 +6,19 @@
 	use Monolog\Handler\StreamHandler;
 	use Monolog\Logger;
 
+	const CRUXOFT_ROOT = ROOT;
+
 	if (PHP_SAPI !== "cli")
 	{
 		die("Command line usage only");
 	}
 
-	require_once(CRUXOFT_ROOT . "vendor/autoload.php");
+	require_once(ROOT . "vendor/autoload.php");
 	
 	set_error_handler([Handler::class, "error"], E_ALL/* & ~E_NOTICE*/);
 	set_exception_handler([Handler::class, "exception"]);
 	
-	Logbook::add(Loggers::GENERAL, array(new StreamHandler(CRUXOFT_ROOT . "logs/main.log", Logger::INFO)));
+	Logbook::add(Loggers::GENERAL, array(new StreamHandler(ROOT . "logs/main.log", Logger::INFO)));
 
 	#putenv("CRUXOFT_DUMP_DEFAULT=" . (Options::ALL & ~Options::DIE_AFTER));
 ?>

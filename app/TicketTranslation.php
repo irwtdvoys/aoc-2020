d0165fc39738bea466a50ebf8acdcceebdcbb16f
Steve Adams <irwtdvoys@gmail.com>
2022-02-07 12:01:21 +0000

    Update to match new run system

--- app/TicketTranslation.php
+++ app/TicketTranslation.php
@@ -11,9 +11,9 @@
 		public array $ticket;
 		public array $nearby;
 
-		public function __construct(int $day, string $override = null)
+		public function __construct(int $day, bool $verbose = false, string $override = null)
 		{
-			parent::__construct($day);
+			parent::__construct($day, $verbose);
 
 			$raw = parent::load($override);
 
